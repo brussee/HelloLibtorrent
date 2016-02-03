b70656a615b041f3a9350f3c30f56532cf4d83bd
Paul Brussee <brussee@live.nl>
2016-02-03 15:43:41 +0100

    NDK_DEBUG=1

--- clean-and-build.sh
+++ clean-and-build.sh
@@ -4,6 +4,6 @@ rm -rf libs &&
 rm -rf obj &&
 rm -rf gen &&
 /opt/android-ndk-r10e/ndk-build clean &&
-/opt/android-ndk-r10e/ndk-build &&
+/opt/android-ndk-r10e/ndk-build NDK_DEBUG=1 &&
 ant clean &&
 ant debug 
