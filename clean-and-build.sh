0d8ad396841f70ef2bf27b2c69d576264523bdc9
Paul Brussee <brussee@live.nl>
2016-02-04 16:35:29 +0100

    more debug flags

--- clean-and-build.sh
+++ clean-and-build.sh
@@ -3,7 +3,7 @@ rm -rf bin &&
 rm -rf libs &&
 rm -rf obj &&
 rm -rf gen &&
-/opt/android-ndk-r10e/ndk-build clean &&
+/opt/android-ndk-r10e/ndk-build NDK_DEBUG=1 clean &&
 /opt/android-ndk-r10e/ndk-build NDK_DEBUG=1 &&
 ant clean &&
 ant debug 
